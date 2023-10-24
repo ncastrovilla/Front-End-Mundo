e5f9a60194b9f999f783a5f9bf9dd21e277ba1e5
Nicolás Castro <ncastrov@ubiobio.cl>
2023-10-24 15:20:17 -0300

    Correccion detalle cambio URL

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,30 +1,31 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Select, Container, Option } from './style'
+import { Select, Container } from './style'
+import { URL, API_KEY } from '../../url'
 
-export const Combobox = ({titulo,url,setVariable,variable = ''}) => {
-    const [datos, setDatos] = useState([])
-    useEffect(() => {
-        setVariable('');
-        axios({
-          method: 'get',
-          url: 'http://127.0.0.1:8000/api/'+url,
-          responseType: 'stream'
-        })
-          .then(function (response) {
-            setDatos(JSON.parse(response.data))
-            
-          });
-  
-      }, [variable])
+export const Combobox = ({ titulo, url, setVariable, variable = '' }) => {
+  const [datos, setDatos] = useState([])
+  useEffect(() => {
+    setVariable('');
+    axios({
+      method: 'get',
+      url: URL + url,
+      responseType: 'stream'
+    })
+      .then(function (response) {
+        setDatos(JSON.parse(response.data))
 
-    return (
+      });
+
+  }, [variable])
+
+  return (
     <Container>
-        <Select onChange={(event)=>setVariable(event.target.value)}>
-      <option value={''}>{titulo}</option>
-      {
-        
-        datos.map(item=>(<option value={item.id} key={item.id}>{item.name}</option>))
+      <Select onChange={(event) => setVariable(event.target.value)}>
+        <option value={''}>{titulo}</option>
+        {
+
+          datos.map(item => (<option value={item.id} key={item.id}>{item.name}</option>))
 
         }</Select>
     </Container>
---
+++ src/url/index.js
@@ -0,0 +1,4 @@
+
+export const URL = 'http://127.0.0.1:8000/api/';
+
+export const API_KEY = '9deb9beb54f1';
